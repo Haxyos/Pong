8c6fa22b1f999758c9c0afacb5c9e12985d4f4a7
Delacoudre Willelm <delacoudrew@gmail.com>
2025-09-24 14:08:40 +0200

    (UPDATE) Changing js back of the page
    CHnaging all the javascript code for more comprehension and adding more fluid fonctiunnalities

--- script/script.js
+++ script/script.js
@@ -1,45 +1,58 @@
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
-
-var keyState = {};  
+var keyState = {};
 let body = document.querySelector("body");
 let canva = document.getElementById("canva");
 let ctx = canva.getContext("2d");
 let leftInput = document.getElementById("leftInput");
 let rightInput = document.getElementById("rightInput");
-
-
-ctx.fillStyle = "darkblue";
-ctx.fillRect(0, 0, canva.width, canva.height);
+let lancer1 = false;
+let lancer2 = false;
 
 let x = canva.width / 2;
 let y = (canva.height / 6) * 5;
 let dx = 0;
-let dy = 5;
+let dy = 0;
 let radius = 6.5;
 let gameLoose = false;
-let rectangleX = canva.width/2-25;
-let rectangleY = canva.height -50;
+let rectangleX = canva.width / 2 - 25;
+let rectangleY = canva.height - 50;
 let rectangleWidth = 60;
 let rectangleHeight = 8;
 let dateDebut = Date.now();
+let id;
+function drawBackground() {
+  ctx.fillStyle = "darkblue";
+  ctx.fillRect(0, 0, canva.width, canva.height);
+}
+
+drawBackground();
 
 function resetCanva() {
-  dx = 0;
-  dy = 5;
-  dateDebut = Date.now();
-  gameLoose = false;
-  ctx.beginPath();
-  ctx.clearRect(0, 0, canva.width, canva.height);
-
-  ctx.fillStyle = "white";
-  ctx.arc(canva.width / 2, (canva.height / 6) * 5, radius, 2 * Math.PI, false);
+  if(lancer1){
+    lancer2 = true;
+  }
+  else{
+    lancer1 = true;
+  }
+  dx = Math.random() * 2 + Math.random() * (-3);
+  dy = -2;
   x = canva.width / 2;
   y = (canva.height / 6) * 5;
-  ctx.fill();
+  rectangleX = canva.width / 2 - 25;
+  rectangleY = canva.height - 50;
+  dateDebut = Date.now();
+  ctx.beginPath();
+  ctx.clearRect(0, 0, canva.width, canva.height);
+  drawBackground();
+  drawBall();
   drawPad();
   ctx.closePath();
+  if (!gameLoose){
+    startGame();
+  }
+  gameLoose = false;
 }
 
 function drawPad() {
@@ -50,10 +63,7 @@ function drawPad() {
   ctx.closePath();
 }
 
-function draw() {
-  if (gameLoose == true) {
-    return;
-  }
+function drawBall() {
   ctx.beginPath();
   ctx.clearRect(0, 0, canva.width, canva.height);
   ctx.fillStyle = "darkblue";
@@ -62,11 +72,25 @@ function draw() {
   ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
   ctx.fill();
   ctx.stroke();
+}
+
+function draw() {
+  if (gameLoose) {
+    cancelAnimationFrame(id);
+    resetCanva();
+    return;
+  }
+  if (lancer2){
+    cancelAnimationFrame(id);
+  }
   x += dx;
   y += dy;
+  drawBall();
   drawPad();
   if (canva.height - y < radius) {
     gameLoose = true;
+    lancer1 = false;
+    lancer2 = false;
   }
 
   if (canva.height - y < radius || y < radius) {
@@ -78,49 +102,53 @@ function draw() {
     dy = dy;
   }
 
-  if (y + radius > rectangleY && x > rectangleX && x < rectangleX + rectangleWidth) {
+  if (
+    y + radius > rectangleY &&
+    x > rectangleX &&
+    x < rectangleX + rectangleWidth
+  ) {
     dx = dx;
-    dy = -dy;
+    dy = -dy - 0.15;
   }
+  console.log(dx, dy);
   let timer = Date.now();
-  console.log(timer - dateDebut);
-  document.getElementById("Score").innerHTML = "Score : " + Math.floor((timer-dateDebut)/1000) + " s";
-  requestAnimationFrame(draw);
+  document.getElementById("Score").innerHTML =
+    "Score : " + Math.floor((timer - dateDebut) / 1000) + " s";
+  id = requestAnimationFrame(draw);
 }
-window.addEventListener('keydown',function(e){
-    keyState[e. keyCode || e.which] = true;
-  },true);    
-window.addEventListener('keyup',function(e){
+window.addEventListener(
+  "keydown",
+  function (e) {
+    keyState[e.keyCode || e.which] = true;
+  },
+  true
+);
+window.addEventListener(
+  "keyup",
+  function (e) {
     keyState[e.keyCode || e.which] = false;
-},true);
-leftInput.addEventListener('click', function(e) {
+  },
+  true
+);
+leftInput.addEventListener("click", function (e) {});
+rightInput.addEventListener("click", function (e) {});
 
-});
-rightInput.addEventListener('click', function(e) {
-});
-
-  function gameLoop() {
-    if (keyState[37] || keyState[65]){
-      if(rectangleX > 0){
-        rectangleX -= 5;
-      }
-    }    
-    if (keyState[39] || keyState[68]){
-      if(rectangleX + rectangleWidth < canva.width){
-        rectangleX += 5;
-      } 
+function gameLoop() {
+  if (keyState[37] || keyState[65]) {
+    if (rectangleX > 0) {
+      rectangleX -= 5;
     }
-    setTimeout(gameLoop,10)
+  }
+  if (keyState[39] || keyState[68]) {
+    if (rectangleX + rectangleWidth < canva.width) {
+      rectangleX += 5;
+    }
+  }
+  setTimeout(gameLoop, 10);
 }
-
-
-
 
 function startGame() {
-  resetCanva();
-  dx = 1
-  dy = -2
   draw();
 }
 
-  gameLoop();
+gameLoop();
